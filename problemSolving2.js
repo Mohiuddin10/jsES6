139e23ff93686333d900b2c52dc840f401c6b694
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-03-26 03:28:02 +0600

    div by 3 added

--- problemSolving2.js
+++ problemSolving2.js
@@ -6,5 +6,14 @@ for (let i = 1; i <= lastNumber; i++) {
         console.log(i)};
 };
 };
-evenNumber(5000);
+// evenNumber(10);
 // console.log(arry);
+let divBy3COunt = [];
+const divBy3 = (start, end) => {
+    for (let i = start; i <= end; i++) {
+        if (i % 3 ==+ 0) {
+            divBy3COunt.push(i);
+            console.log(i)};
+        };};
+divBy3(1, 20);
+console.log(`There are ${divBy3COunt.length} numbers that are divides by 3`);
