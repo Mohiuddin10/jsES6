747ade153418f3a91f903239a6b293b4189afca9
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-03-26 02:51:35 +0600

    Probleming solving session

---
+++ largeVer.js
@@ -0,0 +1,12 @@
+let a = 300;
+let b = 40;
+let c = 5;
+
+if (a > b && a > c) {
+    console.log("A is greater of all numbers");}
+    else if (b > c) {
+        console.log("B is greater of all numbers");
+    }
+    else {
+        console.log("C is greater of all numbers");
+    }
---
+++ problemSolving2.js
@@ -0,0 +1,10 @@
+let arry =[];
+
+const evenNumber = (lastNumber) => {
+for (let i = 1; i <= lastNumber; i++) {
+    if (i % 2 === 0) {
+        console.log(i)};
+};
+};
+evenNumber(5000);
+// console.log(arry);
