5ff5a975aa547dece8663040f88e573e73027a7e
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-03-27 01:24:22 +0600

    new api load

---
+++ touristData.js
@@ -0,0 +1,28 @@
+const data = [
+    {_id : 001,
+    name : 'coxs bazar',
+    district : 'Chattogram',
+    isHill : false,
+    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcoxs-bazar&psig=AOvVaw0pXrcfEkKf6OUghRJoU9FX&ust=1679944734976000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPjgpvun-v0CFQAAAAAdAAAAABAE'},
+
+    {
+        _id : 002,
+    name : 'Bandarban',
+    district : 'Chattogram',
+    isHill : true,
+    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dbandarban&psig=AOvVaw3nmS6OQxEvqOFyUYErAr4l&ust=1679944821756000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCIjMyaSo-v0CFQAAAAAdAAAAABAE'},
+
+
+    {_id : 003,
+    name : 'Jaflong',
+    district : 'Sylet',
+    isHill : true,
+    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fjaflong%252C-sylhet%252C-bangladesh&psig=AOvVaw13Rfd4mJW4fWEV-JxvjTb-&ust=1679944896135000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCJjM98eo-v0CFQAAAAAdAAAAABAE'},
+    
+    {_id : 004,
+    name : 'Kuakata',
+    district : 'Chattogram',
+    isHill : false,
+    img: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fkuakata%252C-bangladesh&psig=AOvVaw3ldPRhCf3DSwHUbS9cU4ch&ust=1679944958243000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCOCwyOWo-v0CFQAAAAAdAAAAABAE'}
+
+];
